696aab16a607126fb9d7fef06eb33bc5f8ce079d
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-09-21 13:45:41 +0300

    Create bastion.tf

---
+++ terraform/bastion.tf
@@ -0,0 +1,33 @@
+resource "yandex_compute_instance" "bastion" {
+  name        = "bastion-host"
+  zone        = "ru-central1-b" # Bastion в зоне B
+  platform_id = "standard-v1"
+  hostname    = "bastion-host"
+
+  resources {
+    cores  = 4
+    memory = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8l04iucc4vsh00rkb1" # ID образа Ubuntu Linux
+      size  = 30
+    }
+  }
+
+  # --- ПЕРВЫЙ СЕТЕВОЙ ИНТЕРФЕЙС (ПУБЛИЧНЫЙ) ---
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public_subnet.id # Public subnet в зоне B
+    security_group_ids = [yandex_vpc_security_group.bastion_sg.id]
+    nat                = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+output "bastion_nat_ip" {
+  value = yandex_compute_instance.bastion.network_interface.0.nat_ip_address
+}
