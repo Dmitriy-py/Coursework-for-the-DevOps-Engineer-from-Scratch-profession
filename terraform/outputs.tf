197c80db497c70dd3ade01900cae3ee22fbb8cf9
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-09-22 10:20:06 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,10 @@ output "alb_external_ip_address" {
   description = "The external IPv4 address of the Application Load Balancer"
   value       = yandex_alb_load_balancer.my_alb.listener.0.endpoint.0.address.0.external_ipv4_address
 }
-
 output "bastion_nat_ip" {
   value = yandex_compute_instance.bastion.network_interface.0.nat_ip_address
+}
+output "loki_internal_ip" {
+  description = "Internal IP address of the Loki VM"
+  value       = yandex_compute_instance.loki_vm.network_interface.0.ip_address
 }
