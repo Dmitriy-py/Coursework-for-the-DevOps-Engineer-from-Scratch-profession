5af3221bacbbe5a121921eebe0901cc3d689dfaa
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-09-24 23:17:11 +0300

    Update snapshot_schedule.tf

--- terraform/snapshot_schedule.tf
+++ terraform/snapshot_schedule.tf
@@ -1,10 +1,7 @@
 # Ресурс для расписания создания снимков дисков
 resource "yandex_compute_snapshot_schedule" "daily_vm_snapshots" {
   name        = "daily-vm-snapshots"
-  folder_id   = "b1gmmll0272r5rs75acl" # Используем переменную folder_id, если она у вас определена
-  # Если нет, замените на ID вашей папки: folder_id = "<ВАШ_ID_ПАПКИ>"
-  # Список ID дисков всех ВМ, для которых будет создаваться расписание
-  # Убедитесь, что здесь перечислены ВСЕ ваши виртуальные машины
+  folder_id   = "b1gmmll0272r5rs75acl"
   disk_ids = [
     yandex_compute_instance.bastion.boot_disk.0.disk_id,
     yandex_compute_instance.web_server_a.boot_disk.0.disk_id,
@@ -24,7 +21,7 @@ resource "yandex_compute_snapshot_schedule" "daily_vm_snapshots" {
   # Политика хранения: снимки хранятся одну неделю (7 дней)
   retention_period = "168h"
 
-  # (Опционально) Задать метки для снимков
+  # метки для снимков
   labels = {
     env = "production"
     backup = "daily"
