96cb0be1b93e37c5e4b171adcb3540e068e20145
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-09-24 23:03:39 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.101.0" # Обновлена версия провайдера
+      version = "~> 0.101.0"
     }
   }
-  required_version = ">= 1.0" # Рекомендуется для актуальных провайдеров
+  required_version = ">= 1.0"
 }
 
 provider "yandex" {
   cloud_id  = "b1gpe6vnl2rgqimj7eo2"
   folder_id = "b1gmmll0272r5rs75acl"
   service_account_key_file = "/home/vm1/yandex_cloud_infra/key.json"
-  zone      = "ru-central1-b" # Убедитесь, что это ваша основная зона
+  zone      = "ru-central1-b"
 }
