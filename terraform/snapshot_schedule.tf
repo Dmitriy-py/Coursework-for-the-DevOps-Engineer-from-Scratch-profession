f19450708827bcf7bf05440dd4033d186e2ab465
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2026-02-11 19:29:56 +0300

    Update snapshot_schedule.tf

--- terraform/snapshot_schedule.tf
+++ terraform/snapshot_schedule.tf
@@ -1,4 +1,3 @@
-# Ресурс для расписания создания снимков дисков
 resource "yandex_compute_snapshot_schedule" "daily_vm_snapshots" {
   name        = "daily-vm-snapshots"
   folder_id   = "b1gmmll0272r5rs75acl"
@@ -11,16 +10,12 @@ resource "yandex_compute_snapshot_schedule" "daily_vm_snapshots" {
     yandex_compute_instance.prometheus_vm.boot_disk.0.disk_id,
   ]
 
-  # Политика расписания: ежедневное копирование
   schedule_policy {
-    # Cron-выражение для ежедневного запуска в полночь (UTC)
     expression = "0 0 * * *"
   }
 
-  # Политика хранения: снимки хранятся одну неделю (7 дней)
   retention_period = "168h"
 
-  # метки для снимков
   labels = {
     env = "production"
     backup = "daily"
