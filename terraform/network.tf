a352d45c9f2553e24f536071bf6b3cb069e8852d
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2026-02-11 19:25:27 +0300

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -7,7 +7,6 @@ resource "yandex_vpc_subnet" "private_subnet_a" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.1.0.0/24"]
-  # Привязка маршрутной таблицы делается прямо здесь
   route_table_id = yandex_vpc_route_table.inner-to-nat.id
 }
 
@@ -16,7 +15,6 @@ resource "yandex_vpc_subnet" "private_subnet_b" {
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.2.0.0/24"]
-  # Привязка маршрутной таблицы делается прямо здесь
   route_table_id = yandex_vpc_route_table.inner-to-nat.id
 }
 
@@ -25,11 +23,8 @@ resource "yandex_vpc_subnet" "public_subnet" {
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.0.0.0/24"]
-  # Публичной подсети обычно не нужна кастомная маршрутная таблица для выхода в интернет.
-  # Она использует default gateway Yandex Cloud.
 }
 
-# Add Route Table
 resource "yandex_vpc_route_table" "inner-to-nat" {
   network_id = yandex_vpc_network.network.id
 
